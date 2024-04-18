1cbcd6c3c3799bce239893298ecffcba3ab6bee5
bbek4210 <bhandaribibek731@gmail.com>
2024-04-18 14:34:54 +0545

    h

--- src/paginated.jsx
+++ src/paginated.jsx
@@ -65,7 +65,7 @@ function Products() {
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="flex items-center justify-between">
             <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-              My store
+              store
             </h2>
           </div>
           <div>
